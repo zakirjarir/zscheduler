56dd9f7db29233b3c1c3d52928370fd7b51e842b
zakirjarir <zakirjarir@gmail.com>
2026-02-16 01:49:57 +0600

    v:1.0.0

--- android/src/main/java/com/zscheduler/app/ZScheduler.java
+++ android/src/main/java/com/zscheduler/app/ZScheduler.java
@@ -2,10 +2,48 @@ package com.zscheduler.app;
 
 import com.getcapacitor.Logger;
 
-public class ZScheduler {
 
-    public String echo(String value) {
-        Logger.info("Echo", value);
-        return value;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.PluginMethod;
+
+
+@CapacitorPlugin(name = "ZScheduler")
+public class ZScheduler extends Plugin {
+
+    private Handler handler;
+    private Runnable runnable;
+
+    @PluginMethod
+    public void startPeriodic(PluginCall call) {
+        int interval = call.getInt("interval", 1000); // milliseconds
+        String eventName = call.getString("eventName", "schedulerEvent");
+
+        handler = new Handler(Looper.getMainLooper());
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                // JS এ event পাঠানো হচ্ছে
+                notifyListeners(eventName, null);
+                handler.postDelayed(this, interval); // next execution
+            }
+        };
+
+        handler.postDelayed(runnable, interval);
+
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void stopPeriodic(PluginCall call) {
+        if (handler != null && runnable != null) {
+            handler.removeCallbacks(runnable);
+        }
+        call.resolve();
     }
 }
